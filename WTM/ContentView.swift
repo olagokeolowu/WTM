e6154436722661a32ca8f82343311a2819dc71a9
jermaine-iris <danieljermaine97@gmail.com>
2025-08-17 19:24:00 +0100

    Initial Commit

---
+++ WTM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WTM
+//
+//  Created by Daniel Jermaine on 17/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WTM/WTMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTMApp.swift
+//  WTM
+//
+//  Created by Daniel Jermaine on 17/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct WTMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
